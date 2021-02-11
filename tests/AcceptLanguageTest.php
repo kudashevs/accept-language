476905199537eb6b0fe75e183d3afe35cfb749f0
Sergey Kudashev <kudashevs@gmail.com>
2021-02-11 19:04:23 +0300

    Update AcceptLanguageTest add getLanguage() test with data provider set.

--- tests/AcceptLanguageTest.php
+++ tests/AcceptLanguageTest.php
@@ -21,4 +21,33 @@ class AcceptLanguageTest extends TestCase
 
         $this->assertSame(AcceptLanguage::DEFAULT_LANGUAGE, $service->getLanguage());
     }
+
+    /**
+     * @dataProvider provideLanguageInformation
+     */
+    public function testGetLanguageReturnsExpected($expected, $options)
+    {
+        $service = new AcceptLanguage($options);
+        $result = $service->getLanguage();
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function provideLanguageInformation()
+    {
+        return [
+            'two letters primary langtag' => [
+                'en',
+                ['accept_language' => 'en'],
+            ],
+            'three letters primary langtag' => [
+                'sgn',
+                ['accept_language' => 'sgn'],
+            ],
+            'sequence of primary tags' => [
+                'de',
+                ['accept_language' => 'de,en-us;q=0.7,en;q=0.3'],
+            ],
+        ];
+    }
 }
