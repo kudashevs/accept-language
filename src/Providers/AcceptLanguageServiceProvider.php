638034745aaa6cf35e1418dc816cfbcf67303d93
Sergey Kudashev <kudashevs@gmail.com>
2021-02-17 16:30:20 +0300

    Add Laravel service provide, update composer.json extra information.

---
+++ src/Providers/AcceptLanguageServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Kudashevs\AcceptLanguage\Providers;
+
+use Illuminate\Container\Container;
+use Illuminate\Support\ServiceProvider;
+use Kudashevs\AcceptLanguage\AcceptLanguage;
+
+class AcceptLanguageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('acceptlanguage', function ($app) {
+            return (new AcceptLanguage($this->getLanguageSettings($app)));
+        });
+
+        $this->app->alias('acceptlanguage', AcceptLanguage::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * @param Container $app
+     * @return array
+     */
+    private function getLanguageSettings(Container $app): array
+    {
+        return array_filter([
+            'default_language' => $app['config']['app.locale'] ?? false,
+            'accepted_languages' => $app['config']['app.accepted_locales'] ?? false,
+        ]);
+    }
+}
