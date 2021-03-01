5fe81ff9861f68144a1648d19c46ec9895d591c0
Sergey Kudashev <kudashevs@gmail.com>
2021-03-01 14:21:13 +0300

    Add LanguageTagTest add basic tests for LanguageTag class.

---
+++ tests/Support/LanguageTagTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Kudashevs\AcceptLanguage\Tests\Support;
+
+use Kudashevs\AcceptLanguage\Support\LanguageTag;
+use PHPUnit\Framework\TestCase;
+
+class LanguageTagTest extends TestCase
+{
+    public function testNormalizeReturnsNotEmpty()
+    {
+        $languageTag = new LanguageTag();
+
+        $this->assertNotEmpty($languageTag->normalize('en'));
+    }
+
+    /**
+     * @dataProvider provideLanguageTag
+     */
+    public function testGetPreferredLanguageReturnsNormalizedLanguageTag($raw, $expected)
+    {
+        $languageTag = new LanguageTag();
+
+        $this->assertSame($expected, $languageTag->normalize($raw));
+    }
+
+    public function provideLanguageTag()
+    {
+        return [
+            'two-letter primary without change' => [
+                'en',
+                'en',
+            ],
+            'three-letter primary without change' => [
+                'dum',
+                'dum',
+            ],
+            'two-letter primary with extlang remove extlang' => [
+                'zh-yue',
+                'zh',
+            ]
+        ];
+    }
+}
