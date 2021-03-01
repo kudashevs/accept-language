537136674b8d87dac82c6c126af6cf0a6cb6aa63
Sergey Kudashev <kudashevs@gmail.com>
2021-03-01 15:37:50 +0300

    Fix AcceptLanguageTest fix some typos in exceptions case descriptions.

--- tests/Support/LanguageTagTest.php
+++ tests/Support/LanguageTagTest.php
@@ -35,51 +35,51 @@ class LanguageTagTest extends TestCase
                 'dum',
                 'dum',
             ],
-            'two-letter primary hyphenated with extlang remove extlang' => [
+            'two-letter tag hyphenated with extlang remove extlang' => [
                 'zh',
                 'zh-yue',
             ],
-            'two-letter primary underscored with extlang remove extlang' => [
+            'two-letter tag underscored with extlang remove extlang' => [
                 'zh',
                 'zh_yue',
             ],
-            'two-letter primary hyphenated with script append script' => [
+            'two-letter tag hyphenated with script append script' => [
                 'sr_Latn',
                 'sr-Latn',
             ],
-            'two-letter primary underscored with script append script' => [
+            'two-letter tag underscored with script append script' => [
                 'sr_Latn',
                 'sr_Latn',
             ],
-            'two-letter primary hyphenated with region append region' => [
+            'two-letter tag hyphenated with region append region' => [
                 'de_AT',
                 'de-AT',
             ],
-            'two-letter primary underscored with region append region' => [
+            'two-letter tag underscored with region append region' => [
                 'de_AT',
                 'de_AT',
             ],
-            'two-letter primary hyphenated with extlang and region append only region' => [
+            'two-letter tag hyphenated with extlang and region append only region' => [
                 'zh_CN',
                 'zh-cmn-CN',
             ],
-            'two-letter primary underscored with extlang and region append only region' => [
+            'two-letter tag underscored with extlang and region append only region' => [
                 'zh_CN',
                 'zh_cmn_CN',
             ],
-            'two-letter primary hyphenated with script and region append both' => [
+            'two-letter tag hyphenated with script and region append both' => [
                 'sr_Latn_RS',
                 'sr-Latn-RS',
             ],
-            'two-letter primary underscored with script and region append both' => [
+            'two-letter tag underscored with script and region append both' => [
                 'sr_Latn_RS',
                 'sr_Latn_RS',
             ],
-            'two-letter primary hyphenated with extlang, script, and region append expected' => [
+            'two-letter tag hyphenated with extlang, script, and region append expected' => [
                 'zh_Hant_CN',
                 'zh-yue-Hant-CN',
             ],
-            'two-letter primary underscored with extlang, script, and region append expected' => [
+            'two-letter tag underscored with extlang, script, and region append expected' => [
                 'zh_Hant_CN',
                 'zh_yue_Hant_CN',
             ],
@@ -99,15 +99,15 @@ class LanguageTagTest extends TestCase
     public function provideExceptionalCase()
     {
         return [
-            'two-letter primary underscored with extlang out of its scope' => [
+            'two-letter tag hyphenated with extlang out of its scope' => [
                 'zh_CN',
                 'zh-cmn-CN-cmn',
             ],
-            'two-letter primary underscored with script out of its scope' => [
+            'two-letter tag hyphenated with script out of its scope' => [
                 'zh_CN',
                 'zh-cmn-CN-Latn',
             ],
-            'two-letter primary hyphenated with region out of its scope' => [
+            'two-letter tag hyphenated with region out of its scope' => [
                 'de_Latn',
                 'de-ext-Latn-CH-1901',
             ],
