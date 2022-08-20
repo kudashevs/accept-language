23daec3322655c6c9f51b94cb2e14055c06558cb
Sergey Kudashev <kudashevs@gmail.com>
2022-08-20 21:08:51 +0300

    Update AcceptLanguageTest add types to the parameters in the can retrieve preferred language with accepted languages test.

--- tests/AcceptLanguageTest.php
+++ tests/AcceptLanguageTest.php
@@ -274,8 +274,10 @@ class AcceptLanguageTest extends TestCase
      * @test
      * @dataProvider provideDifferentAcceptedLanguagesValues
      */
-    public function it_can_retrieve_the_preferred_language_when_the_accepted_languages_is_set($options, $expected)
-    {
+    public function it_can_retrieve_the_preferred_language_when_the_accepted_languages_is_set(
+        array $options,
+        string $expected
+    ) {
         $service = new AcceptLanguage($options);
         $result = $service->getPreferredLanguage();
 
