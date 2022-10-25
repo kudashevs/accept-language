04c78741e54d5347a9c7a6df7e1680191f037373
Sergey Kudashev <kudashevs@gmail.com>
2022-10-25 21:04:26 +0300

    Refactor LanguageQualityNormalizer extract a not acceptable quality constant.

--- src/Normalizers/LanguageQualityNormalizer.php
+++ src/Normalizers/LanguageQualityNormalizer.php
@@ -6,6 +6,12 @@ namespace Kudashevs\AcceptLanguage\Normalizers;
 
 final class LanguageQualityNormalizer implements AbstractQualityNormalizer
 {
+    /**
+     * The weight is normalized to a real number in the range 0 through 1, where 0.001 is the least preferred and
+     * 1 is the most preferred; a value of 0 means "not acceptable". See RFC 7231, Section 5.3.1.
+     */
+    private const NOT_ACCEPTABLE_QUALITY = 0;
+
     public function __construct()
     {
     }
@@ -21,7 +27,7 @@ final class LanguageQualityNormalizer implements AbstractQualityNormalizer
     private function normalizeQuality($quality, float $default)
     {
         if (!isset($quality)) {
-            return 0;
+            return self::NOT_ACCEPTABLE_QUALITY;
         }
 
         return (float)$quality;
