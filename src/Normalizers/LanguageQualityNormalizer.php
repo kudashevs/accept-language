978c8052f2013d001a6375d4724331c25179a276
Sergey Kudashev <kudashevs@gmail.com>
2022-10-25 21:38:03 +0300

    Update LanguageQualityNormalizer update the functionality of the normalizeQuality method.

--- src/Normalizers/LanguageQualityNormalizer.php
+++ src/Normalizers/LanguageQualityNormalizer.php
@@ -26,8 +26,46 @@ final class LanguageQualityNormalizer implements AbstractQualityNormalizer
 
     private function normalizeQuality($quality, float $default)
     {
-        if (!isset($quality)) {
-            return self::NOT_ACCEPTABLE_QUALITY;
+        if ($this->isValidQuality($quality)) {
+            return $this->prepareQuality($quality);
+        }
+
+        if ($this->isUndefinedQuality($quality)) {
+            return $this->prepareQuality($default);
+        }
+
+        if ($this->isEmptyQuality($quality)) {
+            return $this->prepareQuality($default);
+        }
+
+        return self::NOT_ACCEPTABLE_QUALITY;
+    }
+
+    protected function isValidQuality($value): bool
+    {
+        /**
+         * The weight is normalized to a real number in the range 0 through 1, where 0.001 is the least preferred
+         * and 1 is the most preferred; a value of 0 means "not acceptable". See RFC 7231, Section 5.3.1.
+         */
+        return is_numeric($value) &&
+            $value > 0 &&
+            max(min($value, 1), 0.001) === $value;
+    }
+
+    private function isUndefinedQuality($quality): bool
+    {
+        return is_null($quality);
+    }
+
+    private function isEmptyQuality($quality): bool
+    {
+        return is_string($quality) && trim((string)$quality) === '';
+    }
+
+    private function prepareQuality($quality)
+    {
+        if ((int)$quality === 1) {
+            return 1;
         }
 
         return (float)$quality;
--- tests/Normalizers/LanguageQualityNormalizerTest.php
+++ tests/Normalizers/LanguageQualityNormalizerTest.php
@@ -14,4 +14,90 @@ class LanguageQualityNormalizerTest extends TestCase
 
         $this->assertNotEmpty($normalizer->normalize(1, 1.0));
     }
+
+    /**
+     * @test
+     * @dataProvider provideDifferentQualityValues
+     */
+    public function it_can_normalize_a_quality(array $range, $expected)
+    {
+        $normalizer = new LanguageQualityNormalizer();
+
+        $this->assertSame($expected, $normalizer->normalize(...$range));
+    }
+
+    public function provideDifferentQualityValues()
+    {
+        return [
+            'a random string results in the not acceptable' => [
+                ['wrong', 0.5],
+                0,
+            ],
+            'an invalid negative quality results in the not acceptable' => [
+                [-1, 0.5],
+                0,
+            ],
+            'an invalid positive quality results in the provided default' => [
+                [2, 0.5],
+                0,
+            ],
+            'a null quality results in the provided default' => [
+                [null, 1.0],
+                1,
+            ],
+            'an empty quality results in the provided default' => [
+                ['', 0.5],
+                0.5,
+            ],
+            'a valid quality 0.3 results in the quality' => [
+                [0.3, 0.5],
+                0.3,
+            ],
+            'a valid quality 1 results in the quality' => [
+                [1, 0.5],
+                1,
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider providedDifferentQualityBoundaryValues
+     */
+    public function it_can_normalize_at_boundaries(array $range, $expected)
+    {
+        $normalizer = new LanguageQualityNormalizer();
+
+        $this->assertSame($expected, $normalizer->normalize(...$range));
+    }
+
+    public function providedDifferentQualityBoundaryValues()
+    {
+        return [
+            'a negative out of bounds 0.001 results in the not acceptable' => [
+                [-0.001, 0.5],
+                0,
+            ],
+            'a zero value results in the not acceptable' => [
+                [0, 0.5],
+                0,
+            ],
+            'a positive within bounds 0.001 results in the quality' => [
+                [0.001, 0.5],
+                0.001,
+            ],
+            'a positive within bounds 0.999 results in the quality' => [
+                [0.999, 0.5],
+                0.999,
+            ],
+            'a positive withing bounds 1 results in the quality' => [
+                [1, 0.5],
+                1,
+            ],
+            'a positive out of bounds 1.001 results in the not acceptable' => [
+                [1.001, 0.5],
+                0,
+            ],
+        ];
+    }
 }
