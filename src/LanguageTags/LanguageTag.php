454ea427772b591cc912b46a6e0c18f465489cb7
Sergey Kudashev <kudashevs@gmail.com>
2022-10-26 21:27:01 +0300

    Update LanguageTag add a prepareTagWithQuality method.

--- src/LanguageTags/LanguageTag.php
+++ src/LanguageTags/LanguageTag.php
@@ -17,8 +17,44 @@ final class LanguageTag implements AbstractTag
 
     private function initTag(string $tag, $quality): void
     {
-        $this->languageTag = $tag;
-        $this->quality = $quality;
+        [$this->languageTag, $this->quality] = $this->prepareTagWithQuality($tag, $quality);
+    }
+
+    /**
+     * @return array<string,int|float>
+     */
+    private function prepareTagWithQuality(string $tag, $quality): array
+    {
+        if ($this->isValidLanguageTag(trim($tag)) && $this->isValidQuality($quality)) {
+            return $this->generateLanguageTag($tag, $quality);
+        }
+
+        return $this->generateEmptyLanguageTag();
+    }
+
+    private function isValidLanguageTag($value): bool
+    {
+        return $this->isWildcard($value) || strlen($value) >= 2;
+    }
+
+    private function isWildcard(string $header): bool
+    {
+        return $header === '*';
+    }
+
+    private function isValidQuality($value): bool
+    {
+        return $value > 0 && max(min($value, 1), 0.001) === $value;
+    }
+
+    private function generateLanguageTag(string $tag, float $quality): array
+    {
+        return [$tag, $quality]; // @todo add keys
+    }
+
+    private function generateEmptyLanguageTag(): array
+    {
+        return ['', 0]; // @todo add keys
     }
 
     public function getTag(): string
