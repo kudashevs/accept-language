e4ce29498681030e1277d7424be62b301706b8f0
Sergey Kudashev <kudashevs@gmail.com>
2022-10-29 09:59:02 +0300

    Update AbstractQualityNormalizer add a normalizeWithFallback method.

--- src/Normalizers/AbstractQualityNormalizer.php
+++ src/Normalizers/AbstractQualityNormalizer.php
@@ -11,4 +11,14 @@ interface AbstractQualityNormalizer
      * @return int|float
      */
     public function normalize($quality);
+
+    /**
+     * Perform a normalization process and return a normalized quality.
+     * If the quality is not valid the fallback quality value might be used.
+     *
+     * @param int|float|string $quality
+     * @param float $fallback
+     * @return int|float
+     */
+    public function normalizeWithFallback($quality, float $fallback);
 }
