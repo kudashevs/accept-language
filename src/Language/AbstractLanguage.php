8938dde7ca6ce862909ad60b0a88f81d249b3e8d
Sergey Kudashev <kudashevs@gmail.com>
2022-11-01 13:08:37 +0300

    Update languages add an AbstractLanguage abstraction.

---
+++ src/Language/AbstractLanguage.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kudashevs\AcceptLanguage\Language;
+
+interface AbstractLanguage
+{
+    /**
+     * @return string
+     */
+    public function getTag(): string;
+
+    /**
+     * @return int|float
+     */
+    public function getQuality();
+
+    /**
+     * @return bool
+     */
+    public function isValid(): bool;
+}
