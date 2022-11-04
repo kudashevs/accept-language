985f836f6e9ba79e2284a087bd1173cb38205ac1
Sergey Kudashev <kudashevs@gmail.com>
2022-11-04 14:19:56 +0300

    Style AbstractLanguage add descriptions to some docblocks on some methods.

--- src/Language/AbstractLanguage.php
+++ src/Language/AbstractLanguage.php
@@ -7,21 +7,29 @@ namespace Kudashevs\AcceptLanguage\Language;
 interface AbstractLanguage
 {
     /**
+     * Return a language tag.
+     *
      * @return string
      */
     public function getTag(): string;
 
     /**
+     * Return a primary subtag of a language tag.
+     *
      * @return string
      */
     public function getPrimarySubtag(): string;
 
     /**
+     * Return a quality value.
+     *
      * @return int|float
      */
     public function getQuality();
 
     /**
+     * Determine whether the quality value is valid.
+     *
      * @return bool
      */
     public function isValid(): bool;
