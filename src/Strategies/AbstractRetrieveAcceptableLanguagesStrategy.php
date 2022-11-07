3851a1dbeb12ede6d3b5b8317dc0aa71ef774594
Sergey Kudashev <kudashevs@gmail.com>
2022-11-07 20:49:51 +0300

    Update strategies add a AbstractRetrieveAcceptableLanguagesStrategy abstraction.

---
+++ src/Strategies/AbstractRetrieveAcceptableLanguagesStrategy.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kudashevs\AcceptLanguage\Strategies;
+
+use Kudashevs\AcceptLanguage\Language\AbstractLanguage;
+
+interface AbstractRetrieveAcceptableLanguagesStrategy
+{
+    /**
+     * @param array<AbstractLanguage> $languages
+     * @param array<AbstractLanguage> $accepted
+     * @return array<AbstractLanguage
+     */
+    public function retrieve(array $languages, array $accepted): array;
+}
