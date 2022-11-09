c20c8043677a98e4b03aca8cccaa95f7635b30ea
Sergey Kudashev <kudashevs@gmail.com>
2022-11-09 21:58:20 +0300

    Update PreferredLanguagesExactMatchStrategyTest add a can retrieve the exact language from similar test.

--- tests/Strategies/PreferredLanguagesExactMatchStrategyTest.php
+++ tests/Strategies/PreferredLanguagesExactMatchStrategyTest.php
@@ -46,6 +46,27 @@ class PreferredLanguagesExactMatchStrategyTest extends TestCase
     }
 
     /** @test */
+    public function it_can_retrieve_the_exact_matching_language_from_similar_languages()
+    {
+        $languages = [
+            $this->createLanguage('fr', 1),
+            $this->createLanguage('fr-CH', 0.9),
+            $this->createLanguage('fr-Latn-CH', 0.8),
+            $this->createLanguage('fr-fsl-Latn-CH', 0.7),
+        ];
+
+        $accepted = [
+            $this->createLanguage('fr-CH'),
+        ];
+
+        $strategy = new PreferredLanguagesExactMatchStrategy();
+        $result = $strategy->retrieve($languages, $accepted);
+
+        $this->assertCount(1, $result);
+        $this->assertContainsLanguage(['fr-CH', 0.9], $result);
+    }
+
+    /** @test */
     public function it_can_retrieve_the_exact_matching_languages_from_languages()
     {
         $languages = [
