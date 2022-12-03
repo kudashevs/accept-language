7cfd04a45593d2af3b4cdaae45108a09c5c51d34
Sergey Kudashev <kudashevs@gmail.com>
2022-12-03 10:32:27 +0300

    Style AbstractPreferredLanguagesMatchStrategy fix a docblock on the retrieve method.

--- src/Strategies/AbstractPreferredLanguagesMatchStrategy.php
+++ src/Strategies/AbstractPreferredLanguagesMatchStrategy.php
@@ -11,7 +11,7 @@ interface AbstractPreferredLanguagesMatchStrategy
     /**
      * @param array<AbstractLanguage> $languages
      * @param array<AbstractLanguage> $accepted
-     * @return array<AbstractLanguage
+     * @return array<AbstractLanguage>
      */
     public function retrieve(array $languages, array $accepted): array;
 }
