5a02e99af81c179f4529fd8e287e3106f7beaaf7
Sergey Kudashev <kudashevs@gmail.com>
2022-12-03 10:47:21 +0300

    Style AbstractPreferredLanguagesMatchStrategy add a description to the retrieve method.

--- src/Strategies/AbstractPreferredLanguagesMatchStrategy.php
+++ src/Strategies/AbstractPreferredLanguagesMatchStrategy.php
@@ -12,6 +12,8 @@ use Kudashevs\AcceptLanguage\Language\AbstractLanguage;
 interface AbstractPreferredLanguagesMatchStrategy
 {
     /**
+     * Find and return values from languages that correspond to accepted languages using a matching algorithm.
+     *
      * @param array<AbstractLanguage> $languages
      * @param array<AbstractLanguage> $accepted
      * @return array<AbstractLanguage>
