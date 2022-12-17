5f0effedde209bd9c28ec0c6ee30be3ae2ff0a30
Sergey Kudashev <kudashevs@gmail.com>
2022-12-17 20:15:31 +0300

    Update tests add an acceptance AcceptLanguageTest class.

---
+++ tests/Acceptance/AcceptLanguageTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kudashevs\AcceptLanguage\Tests\Acceptance;
+
+use Kudashevs\AcceptLanguage\AcceptLanguage;
+use Kudashevs\AcceptLanguage\Exceptions\InvalidOptionArgumentException;
+use Kudashevs\AcceptLanguage\Facades\AcceptLanguage as AcceptLanguageFacade;
+use Kudashevs\AcceptLanguage\Tests\ExtendedTestCase;
+
+class AcceptLanguageTest extends ExtendedTestCase
+{
+    /** @test */
+    public function an_instance_can_throw_invalid_option_argument_when_a_wrong_option_provided()
+    {
+        $this->expectException(InvalidOptionArgumentException::class);
+        $this->expectExceptionMessage('wrong value');
+
+        new AcceptLanguage(['separator' => 42]);
+    }
+
+    /** @test */
+    public function an_instance_can_retrieve_a_language()
+    {
+        $service = new AcceptLanguage();
+        $service->process();
+
+        $result = $service->getLanguage();
+
+        $this->assertNotEmpty($result);
+        $this->assertSame('en', $result);
+    }
+
+    /** @test */
+    public function a_facade_can_retrieve_a_language()
+    {
+        $result = AcceptLanguageFacade::getLanguage();
+
+        $this->assertNotEmpty($result);
+        $this->assertStringContainsString('en', $result);
+    }
+}
