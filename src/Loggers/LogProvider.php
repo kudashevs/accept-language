7c37b03dd46c4a46d6d8d8854e6d6edb5fc8e3db
Sergey Kudashev <kudashevs@gmail.com>
2023-04-05 21:52:30 +0300

    Update LogProvider add a log method.

--- src/Loggers/LogProvider.php
+++ src/Loggers/LogProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kudashevs\AcceptLanguage\Loggers;
 
+use Kudashevs\AcceptLanguage\Exceptions\InvalidLoggableEvent;
 use Psr\Log\LoggerInterface;
 
 class LogProvider
@@ -16,5 +17,20 @@ class LogProvider
     public function __construct(LoggerInterface $logger)
     {
         $this->logger = $logger;
+    }
+
+    /**
+     * Log an event with the provided context.
+     *
+     * @param string $event
+     * @param array $context
+     *
+     * @throws InvalidLoggableEvent
+     */
+    public function log(string $event, array $context): void
+    {
+        throw new InvalidLoggableEvent(
+            sprintf('The provided event "%s" is invalid.', $event)
+        );
     }
 }
--- tests/Unit/Loggers/LogProviderTest.php
+++ tests/Unit/Loggers/LogProviderTest.php
@@ -2,6 +2,7 @@
 
 namespace Kudashevs\AcceptLanguage\Tests\Unit\Loggers;
 
+use Kudashevs\AcceptLanguage\Exceptions\InvalidLoggableEvent;
 use Kudashevs\AcceptLanguage\Loggers\DummyLogger;
 use Kudashevs\AcceptLanguage\Loggers\LogProvider;
 use PHPUnit\Framework\TestCase;
@@ -14,5 +15,15 @@ class LogProviderTest extends TestCase
         $provider = new LogProvider(new DummyLogger());
 
         $this->assertInstanceOf(LogProvider::class, $provider);
+    }
+
+    /** @test */
+    public function it_can_throw_an_exception_when_a_wrong_event_provided()
+    {
+        $this->expectException(InvalidLoggableEvent::class);
+        $this->expectExceptionMessage('wrong');
+
+        $provider = new LogProvider(new DummyLogger());
+        $provider->log('wrong', []);
     }
 }
