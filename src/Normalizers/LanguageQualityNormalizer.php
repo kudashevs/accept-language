7ab748f5d78c6f20daf661b19efcc1cb1eaef3be
Sergey Kudashev <kudashevs@gmail.com>
2023-06-24 10:08:41 +0300

    Style LanguageQualityNormalizer change comments format

--- src/Normalizers/LanguageQualityNormalizer.php
+++ src/Normalizers/LanguageQualityNormalizer.php
@@ -41,9 +41,7 @@ final class LanguageQualityNormalizer implements QualityNormalizerInterface
      */
     public function normalize($quality)
     {
-        /**
-         * If no "q" parameter is present, the default weight is 1. See RFC 7231, Section 5.3.1.
-         */
+        // If no "q" parameter is present, the default weight is 1. See RFC 7231, Section 5.3.1.
         if ($this->isUndefinedQuality($quality)) {
             return $this->prepareQuality(1);
         }
@@ -62,10 +60,8 @@ final class LanguageQualityNormalizer implements QualityNormalizerInterface
 
     private function isValidQuality($value): bool
     {
-        /**
-         * The weight is normalized to a real number in the range 0 through 1, where 0.001 is the least preferred
-         * and 1 is the most preferred; a value of 0 means "not acceptable". See RFC 7231, Section 5.3.1.
-         */
+        // The weight is normalized to a real number in the range 0 through 1, where 0.001 is the least preferred
+        // and 1 is the most preferred; a value of 0 means "not acceptable". See RFC 7231, Section 5.3.1.
         return is_numeric($value)
             && $value > 0
             && max(min($value, 1), 0.001) === $value;
@@ -82,10 +78,8 @@ final class LanguageQualityNormalizer implements QualityNormalizerInterface
             );
         }
 
-        /**
-         * Since some clients may omit the quality parameter (the value after "q=" in a request header field) and
-         * this is not a serious mistake, we might want to handle this empty value when a fallback is available.
-         */
+        // Since some clients may omit the quality parameter (the value after "q=" in a request header field) and
+        // this is not a serious mistake, we might want to handle this empty value when a fallback is available.
         if ($this->isEmptyQuality($quality)) {
             return $this->prepareQuality(
                 $this->prepareFallback($fallback)
