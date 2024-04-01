69a7f8a89c9458838bbd2cba6e8cc778e316c622
Sergey Kudashev <kudashevs@gmail.com>
2024-04-01 14:11:41 +0300

    Style LanguageQualityNormalizer update some docblocks

--- src/Normalizers/LanguageQualityNormalizer.php
+++ src/Normalizers/LanguageQualityNormalizer.php
@@ -7,8 +7,8 @@ namespace Kudashevs\AcceptLanguage\Normalizers;
 final class LanguageQualityNormalizer implements QualityNormalizerInterface
 {
     /**
-     * The weight is normalized to a real number in the range 0 through 1, where 0.001 is the least preferred and
-     * 1 is the most preferred; a value of 0 means "not acceptable". See RFC 7231, Section 5.3.1.
+     * The weight is normalized to a real number in the range 0 through 1, where 0.001 is the least preferred
+     * and 1 is the most preferred; a value of 0 means "not acceptable". See RFC 7231, Section 5.3.1.
      */
     private const NOT_ACCEPTABLE_QUALITY = 0;
 
