0525b9fd85f7360c7b09c6f63c09faf156d159d6
Sergey Kudashev <kudashevs@gmail.com>
2024-04-03 14:04:06 +0300

    Update tests add a missing AcceptedLanguagesLogPresenterTest test suite

---
+++ tests/Unit/LogProviders/Presenters/AcceptedLanguagesLogPresenterTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kudashevs\AcceptLanguage\Tests\Unit\LogProviders\Presenters;
+
+use Kudashevs\AcceptLanguage\Languages\DefaultLanguage;
+use Kudashevs\AcceptLanguage\LogProviders\Presenters\AcceptedLanguagesLogPresenter;
+use PHPUnit\Framework\TestCase;
+
+class AcceptedLanguagesLogPresenterTest extends TestCase
+{
+    /** @test */
+    public function it_can_present_an_event_with_data(): void
+    {
+        $handler = new AcceptedLanguagesLogPresenter('retrieve_accepted_languages');
+        $presentation = $handler->present(
+            [DefaultLanguage::create('fr-CH', 1), DefaultLanguage::create('fr', 0.9)],
+        );
+
+        $this->assertMatchesRegularExpression('/fr-CH.*retrieve_accepted_languages/', $presentation);
+    }
+
+    /** @test */
+    public function it_can_present_an_event_with_empty_data(): void
+    {
+        $presenter = new AcceptedLanguagesLogPresenter('retrieve_accepted_languages');
+        $presentation = $presenter->present([]);
+
+        $this->assertStringContainsString('empty', $presentation);
+    }
+}
