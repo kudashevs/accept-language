2fc874fa92e53cd15523515271b98718735323af
Sergey Kudashev <kudashevs@gmail.com>
2024-12-13 20:56:33 +0300

    Update AcceptLanguageTest add some README-related tests

--- tests/Acceptance/AcceptLanguageTest.php
+++ tests/Acceptance/AcceptLanguageTest.php
@@ -53,6 +53,19 @@ class AcceptLanguageTest extends ExtendedTestCase
         ]);
     }
 
+    /**
+     * @test
+     * @see /README.md The explanations are in the Usage section see a Note.
+     */
+    public function it_returns_empty_result_when_it_is_not_processed(): void
+    {
+        $service = new AcceptLanguage();
+
+        $language = $service->getLanguage();
+
+        $this->assertEmpty($language);
+    }
+
     /** @test */
     public function it_can_retrieve_a_language(): void
     {
@@ -72,6 +85,194 @@ class AcceptLanguageTest extends ExtendedTestCase
 
         $this->assertNotEmpty($language);
         $this->assertStringContainsString('en', $language);
+    }
+
+    /**
+     * @test
+     * @see /README.md The explanations are in the Options section see Notes.
+     */
+    public function it_can_retrieve_a_non_exact_match_language(): void
+    {
+        $header = 'de, fr;q=0.9, fr-CH;q=0.8, en;q=0.7, *;q=0.5';
+
+        $service = new AcceptLanguage([
+            'http_accept_language' => $header,
+            'accepted_languages' => ['fr', 'en'],
+        ]);
+        $service->process();
+
+        $this->assertSame('fr', $service->getLanguage());
+        $this->assertSame(0.9, $service->getQuality());
+    }
+
+    /**
+     * @test
+     * @see /README.md The explanations are in the Options section see Notes.
+     */
+    public function it_can_retrieve_a_non_exact_match_language_and_a_subtag(): void
+    {
+        $header = 'de, fr;q=0.9, fr-CH;q=0.8, en;q=0.7, *;q=0.5';
+
+        $service = new AcceptLanguage([
+            'http_accept_language' => $header,
+            'accepted_languages' => ['fr-CH', 'en'],
+        ]);
+        $service->process();
+
+        $this->assertSame('fr_CH', $service->getLanguage());
+        $this->assertSame(0.8, $service->getQuality());
+    }
+
+    /**
+     * @test
+     * @see /README.md The explanations are in the Options section see Notes.
+     */
+    public function it_can_retrieve_a_non_exact_match_derivative(): void
+    {
+        $header = 'de, fr-CH;q=0.9, fr;q=0.8, en;q=0.7, *;q=0.5';
+
+        $service = new AcceptLanguage([
+            'http_accept_language' => $header,
+            'accepted_languages' => ['fr', 'en'],
+        ]);
+        $service->process();
+
+        $this->assertSame('fr', $service->getLanguage());
+        $this->assertSame(0.9, $service->getQuality());
+    }
+
+    /**
+     * @test
+     * @see /README.md The explanations are in the Options section see Notes.
+     */
+    public function it_can_retrieve_an_exact_match_language(): void
+    {
+        $header = 'de, fr-CH;q=0.9, fr;q=0.8, en;q=0.7, *;q=0.5';
+
+        $service = new AcceptLanguage([
+            'http_accept_language' => $header,
+            'accepted_languages' => ['fr', 'en'],
+            'exact_match_only' => true,
+        ]);
+        $service->process();
+
+        $this->assertSame('fr', $service->getLanguage());
+        $this->assertSame(0.8, $service->getQuality());
+    }
+
+    /**
+     * @test
+     * @see /README.md The explanations are in the Options section see Notes.
+     */
+    public function it_can_retrieve_an_exact_match_language_and_a_subtag(): void
+    {
+        $header = 'de, fr-CH;q=0.9, fr;q=0.8, en;q=0.7, *;q=0.5';
+
+        $service = new AcceptLanguage([
+            'http_accept_language' => $header,
+            'accepted_languages' => ['fr-CH'],
+            'exact_match_only' => true,
+        ]);
+        $service->process();
+
+        $this->assertSame('fr_CH', $service->getLanguage());
+        $this->assertSame(0.9, $service->getQuality());
+    }
+
+    /**
+     * @test
+     * @dataProvider provideExactMatchCasesFromReadme
+     * @see          /README.md The explanations are in the Options section see Notes.
+     */
+    public function it_can_retrieve_a_language_with_exact_match_options(
+        array $options,
+        string $expectedLanguage,
+        $expectedQuality
+    ): void {
+        $service = new AcceptLanguage($options);
+        $service->process();
+
+        $this->assertSame($expectedLanguage, $service->getLanguage());
+        $this->assertSame($expectedQuality, $service->getQuality());
+    }
+
+    public static function provideExactMatchCasesFromReadme(): array
+    {
+        return [
+            'the header starts with fr-CH, and exact_match_only set to false, and and accept_languages set to fr (derivative)' => [
+                [
+                    'http_accept_language' => 'fr-CH, fr;q=0.9, *;q=0.5',
+                    'exact_match_only' => false,
+                    'accepted_languages' => ['fr'],
+                ],
+                'fr',
+                1,
+            ],
+            'the header starts with fr-CH, and exact_match_only set to false, and and accept_languages set to fr-ch' => [
+                [
+                    'http_accept_language' => 'fr-CH, fr;q=0.9, *;q=0.5',
+                    'exact_match_only' => false,
+                    'accepted_languages' => ['fr-ch'],
+                ],
+                'fr_CH',
+                1,
+            ],
+            'the header starts with fr-CH, and exact_match_only set to true, and and accept_languages set to fr' => [
+                [
+                    'http_accept_language' => 'fr-CH, fr;q=0.9, *;q=0.5',
+                    'exact_match_only' => true,
+                    'accepted_languages' => ['fr'],
+                ],
+                'fr',
+                0.9,
+            ],
+            'the header starts with fr-CH, and exact_match_only set to true, and and accept_languages set to fr-ch' => [
+                [
+                    'http_accept_language' => 'fr-CH, fr;q=0.9, *;q=0.5',
+                    'exact_match_only' => true,
+                    'accepted_languages' => ['fr-ch'],
+                ],
+                'fr_CH',
+                1,
+            ],
+            'the header starts with fr, and exact_match_only set to false, and and accept_languages set to fr' => [
+                [
+                    'http_accept_language' => 'fr, fr-CH;q=0.9, *;q=0.5',
+                    'exact_match_only' => false,
+                    'accepted_languages' => ['fr'],
+                ],
+                'fr',
+                1,
+            ],
+            'the header starts with fr, and exact_match_only set to false, and and accept_languages set to fr-ch' => [
+                [
+                    'http_accept_language' => 'fr, fr-CH;q=0.9, *;q=0.5',
+                    'exact_match_only' => false,
+                    'accepted_languages' => ['fr-ch'],
+                ],
+                'fr_CH',
+                0.9,
+            ],
+            'the header starts with fr, and exact_match_only set to true, and and accept_languages set to fr' => [
+                [
+                    'http_accept_language' => 'fr, fr-CH;q=0.9, *;q=0.5',
+                    'exact_match_only' => true,
+                    'accepted_languages' => ['fr'],
+                ],
+                'fr',
+                1,
+            ],
+            'the header starts with fr, and exact_match_only set to true, and and accept_languages set to fr-ch' => [
+                [
+                    'http_accept_language' => 'fr, fr-CH;q=0.9, *;q=0.5',
+                    'exact_match_only' => true,
+                    'accepted_languages' => ['fr-ch'],
+                ],
+                'fr_CH',
+                0.9,
+            ],
+
+        ];
     }
 
     /** @test */
